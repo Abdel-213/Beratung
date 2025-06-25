a365827ecf7754288e9d0ea9cdb698555c8f823f
A_ELBouanani/ FIAE <apt2@hotmail.de>
2025-06-25 08:14:01 +0200

    .class

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-         Patient patient = new Patient("Werner Lauterbach", 66,"männlich", "1554654668");
-       Berater berater = new Berater("Eisenhauer", 51, "weiblich", "500265");
-       berater.beraten(patient);
-    }
-
-}
-
